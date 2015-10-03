fa7dace85b5fbbc54e6936e39602148cee06b033
Rodrigo Martins <policante.martins@gmail.com>
2015-10-03 17:01:36 -0300

    Initial Commit

---
+++ POLCustomCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POLCustomCamera
+//
+//  Created by Rodrigo Martins on 10/3/15.
+//  Copyright © 2015 Martins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
